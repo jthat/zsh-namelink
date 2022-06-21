8cd6f87fa608acd1e072f1634c1e7e7317b90ded
Jason Thatcher <jason@jthat.com>
2022-06-22 00:17:05 +1000

    Rewrite with improved functionality and documentation

--- README.md
+++ README.md
@@ -1,45 +1,261 @@
 # Namelink zsh plugin
 
-Namelink provides an automatically synchronized mapping of filesystem entries (typically symbolic links) in a set of directories to their counterparts in the named directory hash.
+Namelink manages sets of named directories and keeps them synchronized. One of the main use cases is to manage the zsh named directory hash to facilitate a shared namespace between the `~foo` syntax and external processes like an editor or file manager. This is optional, but enabled by default.
 
-This facilitates a shared namespace between the `~foo` syntax inside zsh, and external processes such as an editor or file manager.
+It works by mapping sets of symbol-path pairs, which consist of associative arrays and/or directories, to likewise sets of output pairs. When the output is a directory, it is managed as a set of symbolic links. By default, the zsh named directory hash is configured as an output associative array, along with the `~/.@` directory.
 
-## ⚠️ WARNING ⚠️
-
-Take care when linking across filesystems that may not always be accessible, such as network volumes, as it has the potential to slow down (or even hang) the shell if the hash needs rebuilding. The author recommends containing such links into a subdirectory beneath the scanned directory to prevent this occurring.
+In order to maximize speed, keep inodes consistent, and minimize churn, namelink strives to minimize reads and writes to the filesystem as much as possible.
 
 ## Directory locations
 
-The default behaviour is to house the entries in the single directory `~/.@`. You can customise this by invoking `namelink_setpath [directory ...]` and passing the set of directories to be scanned from now on.
+Location inputs can also be specified by calling the `namelink load` and `namelink unload` convenience functions. These functions manage the contents of the `namelink` variable (which is included in the set of inputs by default).
 
-## Examples
+## Caveats
 
-### Playing with /usr/local
+Output directories should be considered _fully managed_ by namelink. **Namelink will destructively alter the contents of output directories.**
+
+Take care when linking across filesystems that may not always be accessible, such as network volumes. It has the potential to **slow down or hang the shell** if the cache needs rebuilding. To prevent this occurring, the author recommends containing such links inside a subdirectory which is not directly managed by namelink.
+
+## Configuration
+
+Namelink may be configured by the `namelink setopt` command. For example, to output symbolic links to a folder on the desktop, and disable management of the zsh named directory hash:
 
 ```console
-~% mkdir .@ && cd .@
-~/.@% ln -s /usr/local ul
-~/.@% echo ~ul
-/usr/local
-~/.@% ln -s ul/bin ulb
-~/.@% cd ~ulb && pwd
-/usr/local/bin
-~ulb% rm ~/.@/ulb
-~ul/bin% rm ~/.@/ul
-/usr/local/bin% echo ~ul
-zsh: no such user or named directory: ul
+% namelink setopt outputs dir:~/Desktop/Namelink\ Shortcuts
+% 
 ```
 
-### Desktop GUI convenience
+To search only the `namelink` variable as input, and not the `~/.namelink` directory:
+```console
+% namelink setopt inputs var:namelink
+% 
+```
 
-You may wish to use a parent directory with a human-readable name to enable easier navigation in a desktop context, however a short path lends itself to fewer keystrokes elsewhere. The author uses both:
+### List of configuration variables
+
+| Name | Type | Description | Default |
+| --- | --- | --- | --- |
+| `verbose` | integer | `0` is off, increasing number means more verbosity | `0` |
+| `inputs` | string array | the inputs to synchronize from | `( var:namelink dir:~/.namelinks )` |
+| `outputs` | string array | the outputs to synchronize to | `( var:nameddirs dir:~/.@ )` |
+| `precmd_enable` | boolean | enable `precmd` hook logic | `1` |
+| `preexec_enable` | boolean | enable `preexec` hook logic | `1` |
+| `stat_dirs` | boolean | observe changes in directory modification times in `precmd` and `preexec` hooks | `1` |
+| `always_sync` | boolean | always call `namelink sync` immediately after performing an operation that changes state | `0` |
+
+
+## Tutorial
+
+In this tutorial, we will explore the process of abbreviating `/usr/local` and associated subdirectories.
+
+First, let's create the default input and output directories for namelink to use:
+```console
+% mkdir ~/.namelinks ~/.@
+% 
+```
+
+By default, namelink operates quietly, unless something goes wrong. Let's turn on verbose logging, so we can easily see what namelink is up to:
+```console
+% namelink setopt verbose 1
+% 
+```
+
+Now we are ready to load some mappings. The file `tutorial.namelink` contains:
+```
+# namelink tutorial example
+ul   /usr/local
+ulb  ~ul/bin
+uls  ~ul/sbin
+ulv  ~ul/var
+```
+
+Let's load it:
+```console
+% namelink load < tutorial.namelink
+namelink_sync: rebuilding cache
+namelink_sync_output_dir: ~/.@: CREATE: ulb -> /usr/local/bin
+namelink_sync_output_dir: ~/.@: CREATE: uls -> /usr/local/sbin
+namelink_sync_output_dir: ~/.@: CREATE: ulv -> /usr/local/var
+namelink_sync_output_dir: ~/.@: CREATE: ul -> /usr/local
+% namelink show
+ul /usr/local
+ulb ~ul/bin
+uls ~ul/sbin
+ulv ~ul/var
+% 
+```
+
+These mappings are now available, both in the named directory hash, and as symbolic links in the output directory.
+```console
+% echo ~uls
+/usr/local/sbin
+% realpath ~/.@/uls
+/usr/local/sbin
+% 
+```
+
+`namelink load` supports glob substitution syntax internally. However, if we try to use that syntax directly on the command line, without escaping the `~`, it will be statically expanded before namelink sees it:
+```
+% namelink load uli ~ul/include
+namelink_sync: rebuilding cache
+namelink_sync_output_dir: ~/.@: CREATE: uli -> /usr/local/include
+% namelink show
+ul /usr/local
+ulb ~ul/bin
+uli /usr/local/include
+uls ~ul/sbin
+ulv ~ul/var
+%
+```
+
+If we escape the `~`, however, then namelink will substitute the value when synchronizing:
+```
+% namelink load ule \~ul/etc
+namelink_sync: rebuilding cache
+namelink_sync_output_dir: ~/.@: CREATE: ule -> /usr/local/etc
+% namelink show
+ul /usr/local
+ulb ~ul/bin
+ule ~ul/etc
+uli /usr/local/include
+uls ~ul/sbin
+ulv ~ul/var
+%
+```
+
+Namelink also reads any configured input directories to load mappings:
+```console
+% ln -s ~ul/lib ~/.namelinks/ull
+namelink_stat_dirs: mtime trigger on input dir ~/.namelinks
+namelink_sync_input_dir: loading input path: ~/.namelinks
+namelink_sync: rebuilding cache
+namelink_sync_output_dir: ~/.@: CREATE: ull -> /usr/local/lib
+% namelink show
+ul /usr/local
+ulb ~ul/bin
+ule ~ul/etc
+uli /usr/local/include
+ull /usr/local/lib
+uls ~ul/sbin
+ulv ~ul/var
+% 
+```
+
+This mapping is now available in the same manner as before:
+```console
+% echo ~ull
+/usr/local/lib
+% realpath ~/.@/ull
+/usr/local/lib
+% 
+```
+
+Namelink attempts to keep the output directory synchronized, too. Let's try deleting a file there:
+```console
+% rm ~/.@/ull
+namelink_stat_dirs: mtime trigger on output dir ~/.@
+namelink_sync: rebuilding cache
+namelink_sync_output_dir: ~/.@: CREATE: ull -> /usr/local/lib
+% 
+```
+The damage is repaired.
+
+If we actually want to remove the `ull` mapping, we can simply remove it from the input directory:
+```console
+% rm ~/.namelinks/ull
+namelink_stat_dirs: mtime trigger on input dir ~/.namelinks
+namelink_sync_input_dir: loading input path: ~/.namelinks
+namelink_sync: rebuilding cache
+namelink_sync_output_dir: ~/.@: DELETE: ull
+% 
+```
+
+Recall we can make changes programmatically, too. Let's redefine `uls` to point to `/usr/local/share` instead of `/usr/local/sbin`:
+```console
+% namelink load uls \~ul/share
+namelink_sync: rebuilding cache
+namelink_sync_output_dir: ~/.@: UPDATE: uls -> /usr/local/share
+% namelink show
+ul /usr/local
+ulb ~ul/bin
+ule ~ul/etc
+uli /usr/local/include
+uls ~ul/share
+ulv ~ul/var
+% echo ~uls
+/usr/local/share
+% realpath ~/.@/uls
+/usr/local/share
+% 
+```
+
+And to remove a mapping:
+```console
+% namelink unload ulb
+namelink_sync: rebuilding cache
+namelink_sync_output_dir: ~/.@: DELETE: ulb
+% namelink show
+ul /usr/local
+ule ~ul/etc
+uli /usr/local/include
+uls ~ul/share
+ulv ~ul/var
+% echo ~ulb
+zsh: no such user or named directory: ulb
+% 
+```
+
+That static `uli` definition is bugging me. Let's fix it:
+```console
+% namelink load uli \~ul/include
+namelink_sync: rebuilding cache
+% namelink show
+ul /usr/local
+ule ~ul/etc
+uli ~ul/include
+uls ~ul/share
+ulv ~ul/var
+% 
+```
+That's better.
+
+We can also pass in the original mapping file to `namelink unload`, and namelink will ignore the destination paths therein. When removing entries, namelink will automatically resolve any dangling references.
+```console
+% namelink unload < tutorial.namelink
+namelink_sync: rebuilding cache
+namelink_sync_output_dir: ~/.@: DELETE: ul
+namelink_sync_output_dir: ~/.@: DELETE: uls
+namelink_sync_output_dir: ~/.@: DELETE: ulv
+% namelink show
+ule /usr/local/etc
+uli /usr/local/include
+%
+```
+(The `ulb` mapping wasn't removed here, because we already removed it earlier.)
+
+Finally, to unload everything that is still loaded, we can pass `-a` to `namelink unload`:
+```console
+% namelink unload -a
+namelink_sync: rebuilding cache
+namelink_sync_output_dir: ~/.@: DELETE: ule
+namelink_sync_output_dir: ~/.@: DELETE: uli
+% namelink show
+%
+```
+
+## Desktop GUI convenience
+
+You may wish to use an output directory with a human-readable name to enable easier navigation in a desktop context, however a short path lends itself to fewer keystrokes elsewhere. The author uses both:
 
 ```console
 % cd
 % ls -ld .@ Abbreviations
 lrwxrwxr-x   1 jason  staff    13 25 Aug  2020 .@ -> Abbreviations
 drwxrwxr-x  72 jason  staff  2304 20 Nov 10:17 Abbreviations
+% 
 ```
+
+(This could also have been achieved with multiple output directories.)
 
 For example, on macOS, the abbreviated links can then become meaningfully available when opening a file:
 
--- namelink.zsh
+++ namelink.zsh
@@ -1,52 +1,429 @@
 #
-# zsh plugin for synchronized mapping of filesystem entries to the named directory hash
+# zsh plugin for managing sets of named directories
 #
-# Copyright (C) 2021 Jason Thatcher.
+# Copyright © 2021-2022 Jason Thatcher.
 # Licensed under the MIT license.
 # SPDX-License-Identifier: MIT
 #
 
-zmodload zsh/datetime || { print "can't load zsh/datetime"; return }
-zmodload -F zsh/stat b:zstat || { print "can't load zsh/stat"; return }
-autoload -Uz add-zsh-hook || { print "can't load add-zsh-hook"; return }
+zmodload zsh/datetime || { print "$0: unable to load zsh/datetime" >&2; return 1; }
+zmodload -F zsh/stat b:zstat || { print "$0: unable to load zsh/stat" >&2; return 1; }
+zmodload -F zsh/parameter p:nameddirs || { print "$0: unable to load zsh/parameter" >&2; return 1; }
+autoload -Uz add-zsh-hook || { print "$0: unable to load add-zsh-hook" >&2; return 1; }
 
-namelink_timestamp=0
-(( ${+namelink_path} )) || namelink_path=(~/.@)
+namelink_init() {
+  declare -g -i namelink_conf_verbose=0
+  declare -g -a namelink_conf_inputs=( var:namelink dir:~/.namelink )
+  declare -g -a namelink_conf_outputs=( var:nameddirs dir:~/.@ )
+  declare -g -i namelink_conf_precmd_enable=1
+  declare -g -i namelink_conf_preexec_enable=1
+  declare -g -i namelink_conf_stat_dirs=1
+  declare -g -i namelink_conf_always_sync=0
 
-namelink_hashdir() {
+  declare -g -F namelink_read_timestamp=0
+  declare -g -F namelink_write_timestamp=0
+  declare -g -i namelink_sync_inputs=1
+  declare -g -i namelink_sync_outputs=1
+
+  declare -g -A namelink_cache=()
+  declare -g -A namelink=()
+
+  readonly -g namelink_tilde='~'
+}
+
+namelink_run() {
+  if (( $namelink_conf_verbose > 2 )) {
+    { print -n "$0: "; pwd } >&2
+    setopt localoptions xtrace
+  }
+  $@
+}
+
+namelink_sync_input_dir() {
   setopt local_options extended_glob
-  local d e
-  for d; do
-    for e ($d/*(N))
-      hash -d ${e:t}=${e:P}
-  done 2>/dev/null
+  local index=$1
+  local dir=$2
+  namelink_run builtin cd $dir 2>/dev/null || {
+    (( $namelink_conf_verbose )) && { print "$0: skipping input directory: ${(D)dir}" >&2 }
+    return
+  }
+
+  (( $namelink_conf_verbose )) && { print "$0: loading input directory: ${(D)dir}" >&2 }
+
+  local -A map=()
+  local e
+  for e (*(DN)) {
+    local dest=$e
+    local -A e_info=()
+    # if the entry is a symbolic link, resolve its value
+    if zstat -L -H e_info $e 2>/dev/null && [[ -n $e_info[link] ]]; then
+      dest=${e_info[link]}
+    fi
+    map[$e]=${dest:a}
+  }
+
+  local name=namelink_input_${index}
+  declare -g -A ${name}
+  : ${(AAP)name::=${(@kv)map}}
 }
 
-namelink_rehash() {
-  namelink_timestamp=$EPOCHREALTIME
-  hash -d -r
-  namelink_hashdir $namelink_path
+namelink_sync_input_var() {
+  local index=$1
+  local var=$2
+
+  (( $namelink_conf_verbose )) && { print "$0: loading input variable: ${(D)var}" >&2 }
+
+  local name=namelink_input_${index}
+  declare -g -A ${name}
+  : ${(AAP)name::=${(@kvP)var}}
 }
 
-namelink_rehash_quick() {
-  local d
-  local -A s
+namelink_sync_inputs() {
+  namelink_read_timestamp=$EPOCHREALTIME
+  local orig_pwd=${PWD}
+  local i
+  for (( i=1; i != (${#namelink_conf_inputs}+1); i=$i+1 )); do
+    local input=(${(s.:.)namelink_conf_inputs[$i]})
+    namelink_sync_input_${input[1]} $i ${input[2]}
+  done
+  namelink_run builtin cd $orig_pwd || {
+    print "$0: error: unable to change to original working directory: ${orig_pwd}" >&2
+  }
+  namelink_sync_inputs=0
+}
 
-  for d ($namelink_path) {
-    zstat -F '%s.%9.' -H s $d 2>/dev/null || continue
-    if (( $namelink_timestamp < $s[mtime] )) {
-      namelink_rehash
-      return
+namelink_sync_output_dir() {
+  setopt local_options extended_glob
+  local index=$1
+  local dir=$2
+  namelink_run builtin cd $dir 2>/dev/null || {
+    (( $namelink_conf_verbose )) && { print "$0: skipping output directory: ${(D)dir}" >&2 }
+    return 1
+  }
+
+  (( $namelink_conf_verbose > 3 )) && { command ls -a >&2 }
+
+  local synced=()
+  local IFS=''
+
+  local e
+  for e (*(DN)) {
+    local update=0
+
+    local -A e_info=()
+    zstat -L -H e_info $e 2>/dev/null || {
+      print "$0: error: ${(D)dir}: unable to lstat: ${e}" >&2
+      continue
     }
+
+    if [[ -z $e_info[nlink] ]] {
+      print "$0: warning: ignoring non-symlink ${e} in ${(D)dir}" >&2
+      continue
+    }
+
+    [[ -v namelink_cache[$e] ]] || {
+      (( $namelink_conf_verbose )) && { print "$0: ${(D)dir}: DELETE: ${e}" >&2 }
+      namelink_run command \rm -f -- ${e} || {
+        print "$0: error: unable to delete link in ${(D)dir}: ${e}" >&2
+        continue
+      }
+      namelink_fschange=1
+      continue
+    }
+
+    local v=$namelink_cache[$e]
+
+    # if the link is already correct, skip it
+    if [[ ${e_info[link]} = $v ]] {
+      synced+=($e)
+      (( $namelink_conf_verbose > 1 )) && { print "$0: ${(D)dir}: SKIP: ${e} -> ${v}" >&2 }
+      continue
+    }
+
+    # the link is stale; update it
+    (( $namelink_conf_verbose )) && { print "$0: ${(D)dir}: UPDATE: ${e} -> ${v}" >&2 }
+    namelink_run command \ln -s -n -f -- ${v} ${e} || {
+      print "$0: error: unable to update link in ${(D)dir}: ${e} -> ${v}" >&2
+      continue
+    }
+    synced+=($e)
+    namelink_fschange=1
+  }
+
+  # create the set of links that haven't yet been processed
+  local orphans=(${${(@k)namelink_cache}:|synced})
+  local o
+  for o (${orphans}) {
+    local v=$namelink_cache[$o]
+    (( $namelink_conf_verbose )) && { print "$0: ${(D)dir}: CREATE: ${o} -> ${v}" >&2 }
+    namelink_run command \ln -s -n -- ${v} ${o} || {
+      print "$0: error: unable to create link in ${(D)dir}: ${o} -> ${v}" >&2
+      continue
+    }
+    namelink_fschange=1
+  }
+
+  (( $namelink_conf_verbose > 3 )) && { command ls -a >&2 }
+}
+
+namelink_sync_output_var() {
+  local index=$1
+  local var=$2
+
+  (( $namelink_conf_verbose )) && { print "$0: loading output variable: ${(D)var}" >&2 }
+
+  local name=namelink_input_${index}
+  declare -g -A ${var}
+  : ${(AAP)var::=${(@kv)namelink_cache}}
+}
+
+namelink_sync_outputs() {
+  namelink_write_timestamp=$EPOCHREALTIME
+  local orig_pwd=${PWD}
+  for (( i=1; i != (${#namelink_conf_outputs}+1); i=$i+1 )); do
+    local output=(${(s.:.)namelink_conf_outputs[$i]})
+    namelink_sync_output_${output[1]} $i ${output[2]}    
+  done
+  namelink_run builtin cd $orig_pwd || {
+    print "$0: error: unable to change to original working directory: ${orig_pwd}" >&2
+  }
+  namelink_sync_outputs=0
+}
+
+namelink_sync() {
+  case $1 in -f|--force) namelink_sync_inputs=1; shift;; esac
+
+  (( $namelink_sync_inputs )) || (( $namelink_sync_outputs )) || return
+
+  if (( $namelink_sync_inputs )) {
+    (( $namelink_conf_verbose > 1 )) && { print "$0: input marked sync" >&2 }
+    namelink_sync_inputs
+  }
+
+  (( $namelink_conf_verbose )) && { print "$0: rebuilding cache" >&2 }
+  local i
+  for (( i=1; i != (${#namelink_conf_inputs}+1); i=$i+1 )); do
+    local name=namelink_input_${i}
+    namelink_cache+=(${(@kvP)name})
+  done
+  local k= v= gsv= changed=1
+  while (( $changed )) {
+    changed=0
+    for k v (${(@kv)namelink_cache}) {
+      [[ $v[1] = $namelink_tilde ]] || continue
+      # extract the identifier between '~' and an optional '/'
+      local ref=${${v%%/*}[$((${#namelink_tilde}+1)),-1]}
+      local val=
+      if [[ -z $ref ]] {
+        val=$HOME
+      } else {
+        val=$namelink_cache[$ref]
+      } 
+      if [[ -n $val ]] {
+        # replace the reference with its resolved value
+        namelink_cache[$k]=${val}${v[$((${#namelink_tilde}+${#ref}+1)),-1]}
+        changed=1
+      }
+    }
+  }
+
+  (( $namelink_conf_verbose > 1 )) && { print "$0: output marked sync" >&2 }
+  declare -g -i namelink_fschange=0
+  namelink_sync_outputs
+  if (( $namelink_fschange )) {
+    namelink_sync_outputs
+  }
+  unset namelink_fschange
+}
+
+namelink_stat_dirs() {
+  local i d
+
+  (( $namelink_sync_inputs )) || {
+    for (( i=1; i != (${#namelink_conf_inputs}+1); i=$i+1 )); do
+      local input=(${(s.:.)namelink_conf_inputs[$i]})
+      [[ $input[1] = dir ]] || continue
+      d=$input[2]
+      local -A s=()
+      zstat -F '%s.%9.' -H s $d 2>/dev/null || continue
+      if (( $namelink_read_timestamp < $s[mtime] )) {
+        (( $namelink_conf_verbose )) && { print "$0: mtime trigger on input dir ${(D)d}" >&2 }
+        namelink_sync_inputs=1
+        break
+      }
+    done
+  }
+
+  (( $namelink_sync_inputs )) && namelink_sync_outputs=1
+
+  (( $namelink_sync_outputs )) || {
+    for (( i=1; i != (${#namelink_conf_outputs}+1); i=$i+1 )); do
+      local output=(${(s.:.)namelink_conf_outputs[$i]})
+      [[ $output[1] = dir ]] || continue
+      d=$output[2]
+      local -A s=()
+      zstat -F '%s.%9.' -H s $d 2>/dev/null || continue
+      if (( $namelink_write_timestamp < $s[mtime] )) {
+        (( $namelink_conf_verbose )) && { print "$0: mtime trigger on output dir ${(D)d}" >&2 }
+        namelink_sync_outputs=1
+        break
+      }
+    done
   }
 }
 
-namelink_setpath() {
-  namelink_path=($@)
-  namelink_rehash
+namelink_setopt() {
+  local name=$1
+  shift
+
+  local var=namelink_conf_${name}
+
+  case $name in
+    inputs|outputs)
+      : ${(AP)var::=${(A)@}}
+      local dvar=namelink_sync_${name}
+      : ${(P)dvar::=1}
+      (( $namelink_conf_always_sync )) && namelink_sync
+      return
+      ;;
+  esac
+
+  [[ -v $var ]] || {
+    print "$0: error: unknown option: ${name}" >&2
+    return 1
+  }
+
+  [[ ${(P)var} == $1 ]] && return
+
+  : ${(P)var::=$1}
 }
 
-add-zsh-hook precmd namelink_rehash_quick
-add-zsh-hook preexec namelink_rehash_quick
+namelink_getopt() {
+  local quiet=0; case $1 in -q|--quiet) quiet=1; shift;; esac
 
-namelink_rehash
+  local name=$1
+  shift
+
+  local var=namelink_conf_${name}
+
+  declare -g -a reply=(${(P)var})
+  (( $quiet )) || print ${(j.:.)reply}
+}
+
+namelink_load() {
+  namelink_load_internal() {
+    namelink[$1]=$2
+    namelink_sync_outputs=1
+    return 0
+  }
+
+  if (( $# )) {
+    while (( $# )) {
+      namelink_load_internal $1 $2
+      shift 2
+    }
+  } else {
+    local IFS=''
+    while read -A; do
+      local line=$reply[1]
+      [[ $line =~ '^[[:space:]]*(#.*)?$' ]] && continue
+      [[ $line =~ '^([^[:space:]]+)[[:space:]]+(.*)$' ]] || continue
+      namelink_load_internal $match
+    done
+  }
+
+  unfunction namelink_load_internal
+  (( $namelink_conf_always_sync )) && namelink_sync
+  return 0
+}
+
+namelink_unload() {
+  if [[ $1 = -a ]] {
+    (( ${#namelink} == 0 )) && return 0
+    namelink=()
+    namelink_sync_outputs=1
+    (( $namelink_conf_always_sync )) && namelink_sync
+    return 0
+  }
+
+  namelink_unload_internal() {
+    [[ -v namelink[$1] ]] || return 0
+    local k v
+    for k v (${(@kv)namelink}) {
+      [[ $v[1] = $namelink_tilde ]] || continue
+      # extract the identifier between '~' and an optional '/'
+      local ref=${${v%%/*}[$((${#namelink_tilde}+1)),-1]}
+      if [[ $ref = $1 ]] {
+        # replace the reference with its resolved value
+        namelink[$k]=${namelink[$1]}${v[$((${#namelink_tilde}+${#ref}+1)),-1]}
+      }
+    }
+    unset "namelink[$1]"
+    namelink_sync_outputs=1
+  }
+
+  if (( $# )) {
+    while (( $# )) {
+      namelink_unload_internal $1
+      shift
+    }
+  } else {
+    local IFS=''
+    while read -A; do
+      local line=$reply[1]
+      [[ $line =~ '^[[:space:]]*(#.*)?$' ]] && continue
+      [[ $line =~ '^([^[:space:]]+)([[:space:]]+(.*))?$' ]] || continue
+      namelink_unload_internal ${match[1]}
+    done
+  }
+
+  unfunction namelink_unload_internal
+  (( $namelink_conf_always_sync )) && namelink_sync
+  return 0
+}
+
+namelink_show() {
+  local k=
+  local -A m=(${(@kv)namelink})
+  (( $# == 0 )) && set -- ${(@ko)m}
+  for k; do
+    [[ -v m[$k] ]] || continue
+    print "${k} ${m[$k]}"
+  done
+}
+
+namelink_precmd_hook() {
+  (( $namelink_conf_precmd_enable )) || return 0
+  (( $namelink_conf_verbose > 5 )) && { print "$0" >&2 }
+  (( $namelink_conf_stat_dirs )) && namelink_stat_dirs
+  namelink_sync
+}
+
+namelink_preexec_hook() {
+  (( $namelink_conf_preexec_enable )) || return 0
+  (( $namelink_conf_verbose > 5 )) && { print "$0" >&2 }
+  (( $namelink_conf_stat_dirs )) && namelink_stat_dirs
+  namelink_sync
+}
+
+namelink() {
+  if (( $# == 0 )) {
+    print "$0: missing action" >&2
+    return 1
+  }
+
+  local fun=$1
+  shift
+
+  [[ $(builtin whence -w namelink_${fun}) =~ '[[:space:]]function$' ]] || {
+    print "$0: unknown action: ${fun}" >&2
+    return 1
+  }
+
+  namelink_${fun} $@
+}
+
+add-zsh-hook precmd namelink_precmd_hook
+add-zsh-hook preexec namelink_preexec_hook
+
+namelink_init
